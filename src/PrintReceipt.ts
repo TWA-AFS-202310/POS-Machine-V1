4ed63aa28da5d466f2dc71104ae4e31f33961c71
Diana Jing <DJing@slb.com>
2023-10-31 20:26:54 +0800

    style: remove redundant code

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -6,11 +6,6 @@
 /* eslint-disable prefer-const */
 import {loadAllItems, loadPromotions} from './Dependencies'
 
-interface Quantity{
-  value:number;
-  quantifier:string
-}
-
 interface ReceiptItem{
   barcode:string;
   name:string;
@@ -140,13 +135,4 @@ export function printReceipt(tags: string[]): string {
   const itemsAfterDiscounted = getDiscountedItems(tagWithBarcode)
   return generateItemDetails(itemsAfterDiscounted)
 
-  /*
-  return `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`*/
 }
