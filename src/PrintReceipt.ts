5e7bfc7aaceaf8623a923c77bb55bdb6c1520ef4
Zhiping Shu <ZShu@slb.com>
2023-11-01 09:33:34 +0800

    feat: upload

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -23,16 +23,22 @@ function renderReceipt(receiptItems: ReceiptItem[]): string {
   receiptItems.map(item => { return total += item.subtotal} )
   let discountedPrice = 0
   receiptItems.map(item => { discountedPrice += item.discountedPrice})
+  const str = '***<store earning no money>Receipt ***\n'.concat(
+    receiptItems.map(item => {return `  Name：${item.name}，Quantity：${item.quantity.value} ${item.unit}s，Unit：${item.unitPrice.toFixed(2)}(yuan)，Subtotal：${item.subtotal.toFixed(2)}(yuan)`})
+      .join('\n')
+  ).concat('\n  ----------------------\n').concat(`  Total：${total.toFixed(2)}(yuan)\n`).concat(`  Discounted prices：${discountedPrice.toFixed(2)}(yuan)\n`).concat(`  **********************`)
+  return str
 
-  const subStr1 = '***<store earning no money>Receipt *** \n'
-  const subStr2 =  receiptItems.map(item =>
-  {
-    return `Name：${item.name}，Quantity：${item.quantity.value} ${item.unit}s，Unit：${item.unitPrice.toFixed(2)}(yuan)，Subtotal：${item.subtotal.toFixed(2)}(yuan)`
-  }).join('\n')
-  const subStr3 = `\n----------------------\nTotal：${total.toFixed(2)}(yuan)\n`
-  const subStr4 = `Discounted prices：${discountedPrice.toFixed(2)}(yuan)\n**********************`
+  // const subStr1 = '***<store earning no money>Receipt ***\n'
+  // const subStr2 =  receiptItems.map(item =>
+  // {
+  //   return `Name：${item.name}，Quantity：${item.quantity.value} ${item.unit}s，Unit：${item.unitPrice.toFixed(2)}(yuan)，Subtotal：${item.subtotal.toFixed(2)}(yuan)`
+  // }).join('\n')
+  // const subStr3 = `\n----------------------\nTotal：${total.toFixed(2)}(yuan)\n`
+  // const subStr4 = `Discounted prices：${discountedPrice.toFixed(2)}(yuan)\n**********************`
 
-  return subStr1.concat(subStr2, subStr3, subStr4)
+  // return subStr1.concat(subStr2, subStr3, subStr4)
+
 }
 
 function generateReceiptItems(tags: Tag[]): ReceiptItem[] {
@@ -41,6 +47,7 @@ function generateReceiptItems(tags: Tag[]): ReceiptItem[] {
   const receiptItems: ReceiptItem[] = []
   const promotionBarcode = promotions.find(promote => promote.type ==='BUY_TWO_GET_ONE_FREE')?.barcodes
   for(let i = 0; i < tags.length; i++) {
+    console.log(tags[i])
     const ind = allItems.findIndex(item => tags[i].barcode === item.barcode)
     const name = allItems[ind].name
     const unitPrice = allItems[ind].price
@@ -67,72 +74,71 @@ function calculateDiscountedSubtotal(quantity: number, price: number): number{
   return Math.floor(quantity/3) * price
 }
 
-// function parseTags(tags: string[]): Tag[] {
-//   const parsedTags: Tag[] = []
-//   const barcodeMap = new Map<string, number>()
-//   const tagOrder: string[] = []
-//   for(const tag of tags){
-//     const tagArr = tag.split('-')
-//     if( !barcodeMap.has(tagArr[0]) ){
-//       tagOrder.push(tagArr[0])
-//       if(tagArr.length === 2){
-//         barcodeMap.set(tagArr[0], parseFloat(tagArr[1]))
-//       }else{
-//         barcodeMap.set(tagArr[0], 1)
-//       }
-//     }else {
-//       const recentNum = barcodeMap.get(tagArr[0]) as number
-//       if(tagArr.length === 2){
-//         barcodeMap.set(tagArr[0], recentNum + parseFloat(tagArr[1]) )
-//       }else {
-//         barcodeMap.set(tagArr[0], recentNum + 1)
-//       }
-//     }
-//   }
-//   console.log(barcodeMap)
-//   for(const barcode in tagOrder){
-//     const quantity = barcodeMap.get(barcode) as number
-//     //const tempQuantity = parseQuantity(quantity)
-//     parsedTags.push({
-//       barcode: barcode,
-//       quantity: {
-//         value: quantity,
-//         // quantifier: quantity.toString()
-//       },
-//     })
-//   }
-//   return parsedTags
-// }
-
-
-function parseTags(tags: string[]): Tag[]{
+function parseTags(tags: string[]): Tag[] {
   const parsedTags: Tag[] = []
-  let tempCount = 1
-  let tempQuantity
-  for(let i = 1; i < tags.length; i++){
-    if(tags[i].split('-')[0] === tags[i-1].split('-')[0]){
-      if(tags[i].split('-').length === 2){
-        tempCount += parseFloat(tags[i].split('-')[1])
+  const barcodeMap = new Map<string, number>()
+  const tagOrder: string[] = []
+  for(const tag of tags){
+    const tagArr = tag.split('-')
+    if( !barcodeMap.has(tagArr[0]) ){
+      tagOrder.push(tagArr[0])
+      if(tagArr.length === 2){
+        barcodeMap.set(tagArr[0], parseFloat(tagArr[1]))
       }else{
-        tempCount += 1
+        barcodeMap.set(tagArr[0], 1)
       }
-      continue
     }else {
-      tempQuantity = parseQuantity(tags[i-1], tempCount)
-      parsedTags.push({
-        barcode: tags[i-1].split('-')[0],
-        quantity: tempQuantity
-      })
+      const recentNum = barcodeMap.get(tagArr[0]) as number
+      if(tagArr.length === 2){
+        barcodeMap.set(tagArr[0], recentNum + parseFloat(tagArr[1]) )
+      }else {
+        barcodeMap.set(tagArr[0], recentNum + 1)
+      }
     }
-    tempCount = 1
   }
-  tempQuantity = parseQuantity(tags[tags.length-1], tempCount)
-  parsedTags.push({
-    barcode: tags[tags.length-1].split('-')[0],
-    quantity: tempQuantity
-  })
+
+  for(const barcode of tagOrder){
+    const quantity = barcodeMap.get(barcode) as number
+    parsedTags.push({
+      barcode: barcode,
+      quantity: {
+        value: quantity,
+        quantifier: quantity.toString()
+      },
+    })
+  }
   return parsedTags
 }
+
+
+// function parseTags(tags: string[]): Tag[]{
+//   const parsedTags: Tag[] = []
+//   let tempCount = 1
+//   let tempQuantity
+//   for(let i = 1; i < tags.length; i++){
+//     if(tags[i].split('-')[0] === tags[i-1].split('-')[0]){
+//       if(tags[i].split('-').length === 2){
+//         tempCount += parseFloat(tags[i].split('-')[1])
+//       }else{
+//         tempCount += 1
+//       }
+//       continue
+//     }else {
+//       tempQuantity = parseQuantity(tags[i-1], tempCount)
+//       parsedTags.push({
+//         barcode: tags[i-1].split('-')[0],
+//         quantity: tempQuantity
+//       })
+//     }
+//     tempCount = 1
+//   }
+//   tempQuantity = parseQuantity(tags[tags.length-1], tempCount)
+//   parsedTags.push({
+//     barcode: tags[tags.length-1].split('-')[0],
+//     quantity: tempQuantity
+//   })
+//   return parsedTags
+// }
 
 function parseQuantity(tag: string, num: number): Quantity {
   const allItems = loadAllItems()
@@ -150,7 +156,7 @@ function parseQuantity(tag: string, num: number): Quantity {
   // }
   return {
     value: num,
-    //uantifier: num.toString()
+    quantifier: num.toString()
   }
 
   // return {
@@ -170,6 +176,8 @@ const input = [
   'ITEM000005',
   'ITEM000005-2',
 ]
+
+
 
 
 const tags = parseTags(input)
--- src/interfaces/Quantity.ts
+++ src/interfaces/Quantity.ts
@@ -1,4 +1,4 @@
 export interface Quantity{
     value: number;
-    // quantifier: string;
+    quantifier: string;
 }
